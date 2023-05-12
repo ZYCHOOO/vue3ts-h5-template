340d035d42f234b68e3e0e357b3c756a32cafabd
曾逸超 <zengyichao@digibms.com>
2023-05-12 14:59:45 +0800

    chore(plop)：component 组件自动化构建优化

--- plop-templates/generators/componentGenerator.cjs
+++ plop-templates/generators/componentGenerator.cjs
@@ -7,6 +7,12 @@ module.exports = {
       name: 'componentName', // 作为接收用户输入结果的键
       message: 'please enter component name :',
       default: 'MyComponent'
+    },
+    {
+      type: 'confirm',
+      name: 'globalRegister',
+      message: 'do you want to globally register this component ?',
+      default: 'Y'
     }
   ],
   // 定义生成器在收集完用户的回答后需要进行的任务
@@ -18,6 +24,20 @@ module.exports = {
       path: `src/components/{{componentName}}/index.vue`, // 这里的name就是上面定义的键
       templateFile: 'plop-templates/templates/component.hbs'
     })
+    if (data.globalRegister) {
+      actions.push({
+        type: 'modify',
+        path: 'src/components/index.ts',
+        pattern: /(\/\/ import new components)/gi,
+        template: "import {{ componentName }} from './{{ componentName }}/index.vue'\r\n$1"
+      })
+      actions.push({
+        type: 'modify',
+        path: 'src/components/index.ts',
+        pattern: /(\/\/ add new components)/gi,
+        template: '{{ componentName }},\r\n$1'
+      })
+    }
     return actions
   }
 }
